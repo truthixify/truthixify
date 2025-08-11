5877baf5eb024ccb369860605cd75be51ba7167c
truthixify <truthixify@gmail.com>
2025-08-11 16:45:05 +0100

    updated projects and cv

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -17,6 +17,12 @@ const projectsData: Project[] = [
     description: 'The classic code-breaking game, reimagined for Web3 — powered by Zero-Knowledge Proofs built with for provable fairness. No trust, just truth.',
     imgSrc: '/static/images/mastermind.png',
     href: 'https://word-mastermind.vercel.app/'
+  },
+  {
+    title: 'Obscura',
+    description: 'Obscura is a privacy-focused shielded pool for Starknet, enabling private deposits, transfers, and withdrawals using zero-knowledge proofs and a UTXO-based model.',
+    imgSrc: 'https://obscura-app.vercel.app/logo.png',
+    href: 'https://obscura-app.vercel.app/'
   }
 ]
 
